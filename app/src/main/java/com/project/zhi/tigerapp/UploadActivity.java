0f6e5161c9616662ae2f205773f4983644c126b6
LEAP\George.Zheng <zhi.zheng@leap.com.au>
2018-04-25 18:52:56 +1000

    Upload activity

--- app/src/main/java/com/project/zhi/tigerapp/UploadActivity.java
+++ app/src/main/java/com/project/zhi/tigerapp/UploadActivity.java
@@ -1,8 +1,16 @@
 package com.project.zhi.tigerapp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,12 +20,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.project.zhi.tigerapp.Services.NavigationService;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import lib.folderpicker.FolderPicker;
 
 @EActivity(R.layout.activity_upload)
 public class UploadActivity extends AppCompatActivity
@@ -25,6 +37,17 @@ public class UploadActivity extends AppCompatActivity
 
     @Bean
     NavigationService navigationService;
+
+    private static final int SDCARD_PERMISSION_FOLDER = 12,
+            SDCARD_PERMISSION_FILE = 123,
+            FOLDER_PICKER_CODE = 78,
+            FILE_PICKER_CODE = 786;
+
+    @ViewById(R.id.tv_folder)
+    TextView tv_folderPath;
+
+    @ViewById(R.id.tv_file)
+    TextView tv_filePath;
 
     @AfterViews
     void bindAdapter() {
@@ -93,5 +116,113 @@ public class UploadActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void pickFolder(View v) {
+        pickFolderOrFile(true);
+    }
+
+    public void pickFile(View v) {
+        pickFolderOrFile(false);
+    }
+
+    void pickFolderOrFile(boolean folder) {
+
+        if (Build.VERSION.SDK_INT < 23) {
+
+            if (folder)
+                pickFolder();
+            else
+                pickFile();
+
+        } else {
+
+            if (storagePermissionAvailable()) {
+
+                if (folder)
+                    pickFolder();
+                else
+                    pickFile();
+
+            } else {
+                if (folder) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            SDCARD_PERMISSION_FOLDER);
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            SDCARD_PERMISSION_FILE);
+                }
+            }
+
+        }
+
+    }
+
+    boolean storagePermissionAvailable() {
+        // For api Level 23 and above.
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case SDCARD_PERMISSION_FOLDER:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    pickFolder();
+
+                }
+                break;
+
+            case SDCARD_PERMISSION_FILE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    pickFile();
+
+                }
+                break;
+        }
+    }
+
+
+    void pickFolder() {
+        Intent intent = new Intent(this, FolderPicker.class);
+        startActivityForResult(intent, FOLDER_PICKER_CODE);
+    }
+
+    void pickFile() {
+        Intent intent = new Intent(this, FolderPicker.class);
+
+        //Optional
+
+        intent.putExtra("title", "Select file to upload");
+        intent.putExtra("location", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath());
+        intent.putExtra("pickFiles", true);
+
+        //Optional
+
+        startActivityForResult(intent, FILE_PICKER_CODE);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == FOLDER_PICKER_CODE && resultCode == Activity.RESULT_OK) {
+            String folderLocation = intent.getExtras().getString("data");
+            tv_folderPath.setText(folderLocation);
+        } else if (requestCode == FILE_PICKER_CODE && resultCode == Activity.RESULT_OK) {
+            String folderLocation = intent.getExtras().getString("data");
+            tv_filePath.setText(folderLocation);
+        }
     }
 }
