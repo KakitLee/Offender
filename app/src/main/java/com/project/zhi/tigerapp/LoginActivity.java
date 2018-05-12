21c0d30af33cd772c0e8a9e1dcdce459a7f36585
Es <es930119@gmail.com>
2018-05-12 18:03:17 +1000

    upload
    get username and password success

--- app/src/main/java/com/project/zhi/tigerapp/LoginActivity.java
+++ app/src/main/java/com/project/zhi/tigerapp/LoginActivity.java
@@ -2,6 +2,9 @@ package com.project.zhi.tigerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -10,13 +13,14 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-//    }
     @AfterViews
     void setup(){
 
+    }
+
+    public void getToken(View v){
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        Toast.makeText(LoginActivity.this,username.getText().toString()+password.getText().toString(),Toast.LENGTH_SHORT).show();
     }
 }
